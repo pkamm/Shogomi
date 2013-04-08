e7d8cdab2238dec58d3be820a3b79a4b2b50ef4b
Pete Kamm <sprizzla@gmail.com>
2013-04-08 17:17:29 -0400

    Initial Commit

---
+++ Shogomi/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Shogomi
+//
+//  Created by Peter Kamm on 4/8/13.
+//  Copyright (c) 2013 Smatterbox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
